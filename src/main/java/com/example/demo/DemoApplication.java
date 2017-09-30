58e6cba09c0f4c547c35230ee5b30cf4bdc30ebd
five.liu <five.liu@harmontronics.com>
2017-09-30 14:40:01 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+
+/**
+ * 启动类需要添加Servlet的支持
+ */
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int age;
+}
